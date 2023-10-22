0d8955c94643214baa184439cb48ec9d492b24f3
Edgar767 <utm20040079@utma.edu.mx>
2023-10-21 19:58:39 -0600

    useEffect

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,18 +1,47 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function ListItem({text, checked, onDelete}){
 
-    const[setChecked]=useState(false);
-    const handleCheck = () => {
-        setChecked(!checked)
-    }
 
-    return (
-        <li className={`"p-2 bg-indigo-900" ${checked} ? 'line-throut'" : null`}>
-            {text}<input className="m-2" type="checkbox" checked={checked} onChange={handleCheck}></input>
-        </li>
-    )
-    
+function ListItem({ id, checked, text, onDelete }) {
+  const [isChecked, setChecked] = useState(checked);
+
+
+
+  useEffect(() => {
+    // Inicio de componente
+    console.log("Init");
+    return () => {
+      //Destruye el componente
+      console.log("cleanup");
+    };
+  }, []);
+
+
+
+  useEffect(() => {
+    console.log("checked", checked);
+    if (checked) alert("Check");
+  }, [checked]);
+
+  const handleCheck = () => {
+    setChecked(!isChecked);
+  };
+
+
+  return (
+    <li className={`p-2 bg-[#374151] rounded-md flex justify-between items-center ${isChecked ? 'line-through' : ''}`}>
+      <div>
+        <input className="m-2" type="checkbox" checked={isChecked} onChange={handleCheck} />
+        {text}
+      </div>
+      
+      <div>
+        <button onClick={() => onDelete(id)} className="rounded-md bg-[#b81d1d] px-2 py-1 hover:bg-red-900">
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 }
 
 export default ListItem;
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,18 +1,65 @@
+import { useState, useRef, useEffect } from "react";
 import ListItem from "../components/Listitem";
+import { v4 as uuidv4 } from 'uuid';
 
 
-function Todo(){
-    return (
-    <div>
-        <ul>
-            <ListItem text="Item 1" />
-            <ListItem text="Item 2" />
-            <ListItem text="Item 3" />
-        </ul>
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+
+  useEffect(()=>{
+    const getTodos = () =>{
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then((response)=> response.json())
+        .then((data)=>{
+          console.log(data);
+        })
+    };
+    getTodos();
+  },[]);
+
+
+  // UseEffect ejecuta efectos de los componentes
+  useEffect(()=>{
+    console.log("useEffect",todos);
+  }, [todos])
+
+
+  // Nueva tarea
+  const addTodo = () => {
+    const todoValue = inputRef.current.value;
+    const newTodo = {name: todoValue, id: uuidv4(), checked: false};
+    console.log("before", todos);
+    setTodos([newTodo, ...todos]);
+    console.log("after", todos);
+    inputRef.current.value = "";
+  };
+
+  // Borrar tarea
+  const deleteTodo = (id) => {
+    // Filtra las tareas y Actualiza el estado
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="">
+        <input ref={inputRef} type="text" className="rounded-md bg-[#6b7280] px-4 py-2 mt-3" />
+        <button onClick={addTodo} className="rounded-md bg-[#2563eb] ml-4 px-4 py-2 hover:bg-blue-900">
+          Add task
+        </button>
+      </div>
+      
+      <ul className="flex flex-col gap-3">
+        {todos.map((todo) => (
+          <ListItem key={todo.id} id={todo.id} text={todo.name} onDelete={() => deleteTodo(todo.id)} />
+        ))}
+      </ul>
     </div>
-    
-    )
-    
-
+  );
 }
+
 export default Todo;
